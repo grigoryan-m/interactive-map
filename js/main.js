9e47438c2d97a46395b3df6d922a10bc3146ff85
grigoryan-m <waldm4rk@gmail.com>
2025-03-01 17:43:47 +0400

    hotfix

--- js/main.js
+++ js/main.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     burgerIcon.addEventListener("click", () => {
         menu.classList.toggle("active");
     });
+    class Marker{
+        constructor(lat, lng, cityName, pageName){
+            let marker = L.marker([lat, lng]).addTo(map);
+            marker.bindPopup("<a href='./cities/" + pageName + ".html'>" + cityName + "</a>");
+        }
+    }
+    class ImageMarker{
+        constructor(lat, lng, imageSrc){
+            let marker = L.marker([lat, lng]).addTo(map);
+            marker.bindPopup("<a href='" + imageSrc + "'><img src='" + imageSrc + "' width='200' height='200' /></a>");
+        }
+    }
 });
 
-class Marker{
-    constructor(lat, lng, cityName, pageName){
-        let marker = L.marker([lat, lng]).addTo(map);
-        marker.bindPopup("<a href='./cities/" + pageName + ".html'>" + cityName + "</a>");
-    }
-}
-class ImageMarker{
-    constructor(lat, lng, imageSrc){
-        let marker = L.marker([lat, lng]).addTo(map);
-        marker.bindPopup("<a href='" + imageSrc + "'><img src='" + imageSrc + "' width='200' height='200' /></a>");
-    }
-}
