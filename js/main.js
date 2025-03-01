cea60ab2155986711d917e9a6566a2d39e250a7e
grigoryan-m <waldm4rk@gmail.com>
2025-03-01 17:41:24 +0400

    added zunitrulat

--- js/main.js
+++ js/main.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("active");
     });
 });
+
+class Marker{
+    constructor(lat, lng, cityName, pageName){
+        let marker = L.marker([lat, lng]).addTo(map);
+        marker.bindPopup("<a href='./cities/" + pageName + ".html'>" + cityName + "</a>");
+    }
+}
+class ImageMarker{
+    constructor(lat, lng, imageSrc){
+        let marker = L.marker([lat, lng]).addTo(map);
+        marker.bindPopup("<a href='" + imageSrc + "'><img src='" + imageSrc + "' width='200' height='200' /></a>");
+    }
+}
