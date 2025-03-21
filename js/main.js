78223d43d65e6f0ab41bf85f4512213f10c91263
BeOne4k <beoone555@gmail.com>
2025-03-21 21:04:13 +0400

    class tree added

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const burgerIcon = document.getElementById("burgerIcon");
-    const menu = document.getElementById("menu");
-
-    burgerIcon.addEventListener("click", () => {
-        menu.classList.toggle("active");
-    });
-});
-
