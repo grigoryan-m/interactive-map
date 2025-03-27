106613c71aadd73764113712cff36b5e6fb59cee
grigoryan-m <waldm4rk@gmail.com>
2025-03-27 15:27:03 +0400

    oklurg done, rogue tree done, bug fixes

---
+++ js/header.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const headerHTML = `
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/pages/defaultmap.html">Карта</a></li>
+                    <li><a href="/pages/bestiary.html">Бестиарий</a></li>
+                    <li><a href="/pages/trees.html">Классы</a></li>
+                    <li><a href="/pages/races.html">Расы</a></li>
+                    <li><a href="/pages/currencies.html">Валюты</a></li>
+                </ul>
+            </nav>
+        </header>
+    `;
+
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+});
