a338c78e6f047275d0df9b7d7ddd2f4d41250bc4
grigoryan-m <waldm4rk@gmail.com>
2025-04-22 02:35:28 +0400

    working on relative paths

--- js/header.js
+++ js/header.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
         <header>
             <nav>
                 <ul>
-                    <li><a href="/pages/defaultmap.html">Карта</a></li>
-                    <li><a href="/pages/bestiary.html">Бестиарий</a></li>
-                    <li><a href="/pages/trees.html">Классы</a></li>
-                    <li><a href="/pages/races.html">Расы</a></li>
-                    <li><a href="/pages/currencies.html">Валюты</a></li>
-                    <li><a href="/pages/backgrounds.html">Предыстории</a></li>
-                    <li><a href="/pages/secrets.html">Секреты</a></li>
+                    <li><a href="pages/defaultmap.html">Карта</a></li>
+                    <li><a href="pages/bestiary.html">Бестиарий</a></li>
+                    <li><a href="pages/trees.html">Классы</a></li>
+                    <li><a href="pages/races.html">Расы</a></li>
+                    <li><a href="pages/currencies.html">Валюты</a></li>
+                    <li><a href="pages/backgrounds.html">Предыстории</a></li>
+                    <li><a href="pages/secrets.html">Секреты</a></li>
                 </ul>
             </nav>
         </header>
